71e7ef34093fa8a677d7bc44365faff544977e08
karthi89ck <karthi89ck@gmail.com>
2022-06-13 22:13:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '0.0.0.0';
-const port = 80;
+const port = 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
